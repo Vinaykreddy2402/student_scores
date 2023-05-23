0ccdfa18b9ce0861c49b29c12c6ad680b8aa755a
Vinaykreddy2402 <vinayreddykappeta@gmail.com>
2023-05-23 10:56:45 +0530

    first new commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+Hypen_E_Dot = '-e.'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('/n','')for req in requirements ]
+
+        if Hypen_E_Dot in requirements:
+            requirements.remove(Hypen_E_Dot)
+    return requirements
+    
+
+setup(
+
+    name = "Vinay ML Project",
+    version = '0.0.1',
+    author = 'Vinay',
+    author_email = 'vinayreddykappeta@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+)
